d24da20ad4c50db027fb1f8822efa4ef10938060
Polina Pichugina <p.cipileva@mail.ru>
2024-02-12 16:36:36 +0300

    make gcd game

--- README.md
+++ README.md
@@ -3,3 +3,4 @@
 [![Maintainability](https://api.codeclimate.com/v1/badges/f18ce5cb38c2275776ca/maintainability)](https://codeclimate.com/github/PolinaPichugina/frontend-project-44/maintainability)
 
 https://asciinema.org/a/yS0h42DequKUDQOylfLlllSlv
+https://asciinema.org/a/VhxUcEnbBU8z1cM0mVKwM3lJq
---
+++ src/games/gcd.js
@@ -0,0 +1,19 @@
+import brainGames from '../index.js';
+import getRandomNumber from '../getRandomNumber.js';
+
+const rule = 'Find the greatest common divisor of given numbers.';
+const getQuestionAndAnswer = () => {
+  const a = getRandomNumber();
+  const b = getRandomNumber();
+  const question = `${a} ${b}`;
+  let correctAnswer = 1;
+  for (let i = 2; i < ((a < b) ? a : b); i += 1) {
+    if (a % i === 0 && b % i === 0) {
+      correctAnswer = i;
+    }
+  } return [question, correctAnswer];
+};
+const gcd = () => {
+  brainGames(rule, getQuestionAndAnswer);
+};
+export default gcd;
